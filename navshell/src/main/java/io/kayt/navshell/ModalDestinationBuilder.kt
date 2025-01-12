c88bbf7b93d72cd06020d4eb03671c14525daeac
Mohammad Fallah <mohammad@shape.dk>
2025-01-12 19:16:50 +0100

    Update sample

--- navshell/src/main/java/io/kayt/navshell/ModalDestinationBuilder.kt
+++ navshell/src/main/java/io/kayt/navshell/ModalDestinationBuilder.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.animation.SizeTransform
 import androidx.compose.runtime.Composable
+import androidx.navigation.NamedNavArgument
 import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavDeepLink
 import androidx.navigation.NavGraphBuilder
@@ -46,6 +47,51 @@ inline fun <reified T : Any> NavGraphBuilder.modal(
             content
         )
             .apply {
+                deepLinks.forEach { deepLink -> deepLink(deepLink) }
+                this.enterTransition = enterTransition
+                this.exitTransition = exitTransition
+                this.popEnterTransition = popEnterTransition
+                this.popExitTransition = popExitTransition
+                this.sizeTransform = sizeTransform
+            }
+    )
+}
+
+
+fun NavGraphBuilder.modal(
+    route: String,
+    arguments: List<NamedNavArgument> = emptyList(),
+    deepLinks: List<NavDeepLink> = emptyList(),
+    enterTransition:
+    (@JvmSuppressWildcards
+    AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition?)? =
+        null,
+    exitTransition:
+    (@JvmSuppressWildcards
+    AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition?)? =
+        null,
+    popEnterTransition:
+    (@JvmSuppressWildcards
+    AnimatedContentTransitionScope<NavBackStackEntry>.() -> EnterTransition?)? =
+        enterTransition,
+    popExitTransition:
+    (@JvmSuppressWildcards
+    AnimatedContentTransitionScope<NavBackStackEntry>.() -> ExitTransition?)? =
+        exitTransition,
+    sizeTransform:
+    (@JvmSuppressWildcards
+    AnimatedContentTransitionScope<NavBackStackEntry>.() -> SizeTransform?)? =
+        null,
+    content: @Composable AnimatedContentScope.(NavBackStackEntry) -> Unit
+) {
+    destination(
+        ModalComposeNavigatorDestinationBuilder(
+            provider[ModalComposeNavigator::class],
+            route,
+            content
+        )
+            .apply {
+                arguments.forEach { (argumentName, argument) -> argument(argumentName, argument) }
                 deepLinks.forEach { deepLink -> deepLink(deepLink) }
                 this.enterTransition = enterTransition
                 this.exitTransition = exitTransition
--- sample/src/main/java/io/kayt/navshell/sample/MainActivity.kt
+++ sample/src/main/java/io/kayt/navshell/sample/MainActivity.kt
@@ -1,15 +1,35 @@
 package io.kayt.navshell.sample
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import io.kayt.navshell.NavShell
+import io.kayt.navshell.modal
 import io.kayt.navshell.sample.ui.theme.NavShellTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +38,49 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavShellTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                MainUi()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainUi() {
+    val navController = rememberNavController()
+    NavShell(navController) {
+        Scaffold(
+            bottomBar = {
+                AppBottomBar()
+            }
+        ) { paddingValues ->
+            NavHost(
+                navController = navController,
+                startDestination = "ModalThird",
+                modifier = Modifier.padding(paddingValues)
+            ) {
+                composable("First") {
+                    SampleScreen(
+                        containerColor = Color.Green,
+                        screenTitle = "First screen"
+                    ) {
+                        navController.navigate("Second")
+                    }
+                }
+                composable("Second") {
+                    SampleScreen(
+                        containerColor = Color.Red,
+                        screenTitle = "Second screen"
+                    ) {
+                        navController.navigate("ModalThird")
+                    }
+                }
+                modal("ModalThird") {
+                    SampleScreen(
+                        containerColor = Color.Black,
+                        screenTitle = "MODAL"
+                    ) {
+                        navController.navigate("First")
+                    }
                 }
             }
         }
@@ -30,17 +88,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SampleScreen(
+    containerColor: Color,
+    screenTitle: String,
+    onButtonClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(containerColor),
+        contentAlignment = Alignment.Center
+    ) {
+        Column {
+            Text(screenTitle, color = Color.Black)
+            Button(
+                onClick = onButtonClick,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White
+                )
+            ) {
+                Text("Click on me")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    NavShellTheme {
-        Greeting("Android")
-    }
+fun AppBottomBar() {
+    BottomAppBar(
+        containerColor = Color.Transparent,
+        actions = {
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.Check,
+                    contentDescription = "Localized description"
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.Edit,
+                    contentDescription = "Localized description",
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.Add,
+                    contentDescription = "Localized description",
+                )
+            }
+            IconButton(onClick = { /* do something */ }) {
+                Icon(
+                    Icons.Filled.Delete,
+                    contentDescription = "Localized description",
+                )
+            }
+        })
 }
