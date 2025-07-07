a69c3c2e24048b5731df6a99127ac850de9dda89
Mohammad Fallah <mohammad@shape.dk>
2025-07-07 12:36:31 +0200

    Fix NavShell bug that cause the LayoutInspector crash

--- navshell/src/main/java/io/kayt/navshell/NavShell.kt
+++ navshell/src/main/java/io/kayt/navshell/NavShell.kt
@@ -1,26 +1,16 @@
 package io.kayt.navshell
 
-import androidx.activity.compose.PredictiveBackHandler
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavController
-import androidx.navigation.Navigator
-import androidx.navigation.compose.ComposeNavigator
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.get
-import kotlin.coroutines.cancellation.CancellationException
+import kotlinx.coroutines.flow.first
 
 @Composable
 fun NavShell(
@@ -42,6 +32,8 @@ fun NavShell(
         // Add ModalHost to the composition after the first full composition
         // to let the NavHost to set the graph to the NavController
         LaunchedEffect(Unit) {
+            // suspend until setGraph is called by NavHost (that is inside content())
+            navController.currentBackStackEntryFlow.first()
             // Check that the graph is set by calling NavHost in content()
             runCatching { navController.graph }.onFailure { error("NavHost must be initialized within a NavShell and configured with a valid navigation graph.") }
             showModalHost = true
